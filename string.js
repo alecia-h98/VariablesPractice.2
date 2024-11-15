d3df385f0c0f0b6f503849ffb49f8a3a4ffae3f3
Alecia Hagen <alecia.h98@gmail.com>
2024-11-14 20:18:33 -0600

    finished sring.js file

---
+++ string.js
@@ -0,0 +1,21 @@
+console.log( 'js' );
+
+let catName = 'Jynxies';
+let catAge = 10;
+console.log( catName );
+// concatenation with comma for console (comma only works in console.log)
+console.log( 'The name of the cat is:', catName );
+console.log( 'The age of the cat is:', catAge );
+
+// concatenate text with "+"
+let sentence = 'This is a sentence about the cat named ' + catName;
+console.log( sentence );
+
+sentence = 'The cat is ' + catAge + ' years old.';
+console.log( sentence );
+
+catName = 'Zepher';
+catAge = 2;
+
+sentence = 'Later on, a different cat is ' + catAge + ' old and is named ' + catName;
+console.log( sentence );
